eb148adf51fbbf3dc8904568e993ec76b83c1d54
Charanjit55588 <charanjit55588@gmail.com>
2019-05-03 00:19:47 -0400

    test class

---
+++ src/test/java/com/mycompany/app/Example.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Circle Ci is successful");
+	}
+
+}
